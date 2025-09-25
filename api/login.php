b6bebaf47c0ddc8cb196f422cc591f3181e8474e
جين هيوك <a123@123ui-MacBookPro.local>
2025-09-25 17:35:34 +0900

    complete api

--- api/login.php
+++ api/login.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+require 'db.php';
+
 
 $users = [
     "admin" => "1234",
@@ -9,6 +11,24 @@ $users = [
 // POST 데이터 가져오기
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
+
+
+
+$stmt = $pdo->prepare("SELECT * FROM user WHERE username = ?");
+$stmt->execute([$username]);
+$user = $stmt->fetch();
+
+if($user && password_verify($password, $user['password'])){
+    $_SESSION['username'] = $user['username'];
+    $_SESSION['user_id'] = $user['id'];
+    header("Location: /index.php");
+    exit;
+} else {
+    $_SESSION['error'] = "아이디 또는 비밀번호가 틀렸습니다.";
+    header("Location: /login_form.php");
+    exit;
+}
+
 
 // 로그인 검증
 if(isset($users[$username]) && $users[$username] === $password){
--- api/register.php
+++ api/register.php
@@ -1,19 +1,30 @@
 <?php
 session_start();
-
+require 'db.php';
 
 
 // POST 데이터 가져오기
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
 $password_confirm = $_POST['password'] ?? '';
-// 로그인 검증
-if(isset($username) && $password === $password_confirm){
-    $_SESSION['username'] = $username;
-    header("Location: /index.php");
+
+# 비번 일치확인
+if($password !== $password_confirm){
+    $_SESSION['error'] = "비밀번호가 일치하지 않습니다.";
+    header("Location: register_form.php");
     exit;
-}else{
-    $_SESSION['error'] = "아이디 또는 비밀번호가 틀렸습니다.";
-    header("Location: /login_form.php");
+}
+
+$hash = password_hash ($password, PASSWORD_DEFAULT);
+
+try {
+    $stmt = $pdo->prepare("INSERT INTO user (username, password) VALUES (?, ?)");
+    $stmt->execute([$user_id, $hash, $name]);
+    $_SESSION['success'] = "회원가입 완료!";
+    header("Location: login_form.php");
+    exit;
+} catch (PDOException $e) {
+    $_SESSION['error'] = "회원가입 실패: " . $e->getMessage();
+    header("Location: register_form.php");
     exit;
 }
